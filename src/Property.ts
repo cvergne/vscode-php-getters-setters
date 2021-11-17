5b2d46d22ada5636bff1043541ba66fd9f480dd4
Norber <nox_nox_@hotmail.com>
2021-11-17 10:32:42 +0000

    Fixing linter auto format

--- src/Property.ts
+++ src/Property.ts
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 export default class Property {
     private description: string = null;
@@ -8,17 +8,10 @@ export default class Property {
     private name: string;
     private type: string = null;
     private typeHint: string = null;
-    private pseudoTypes = [
-        "mixed",
-        "number",
-        "callback",
-        "array|object",
-        "void",
-        "null",
-        "integer",
-    ];
+    private pseudoTypes = ['mixed', 'number', 'callback', 'array|object', 'void', 'null', 'integer'];
 
-    public constructor(name: string) {
+    public constructor(name: string)
+    {
         this.name = name;
     }
 
@@ -62,56 +55,36 @@ export default class Property {
         return null;
     }
 
-    static fromEditorPosition(
-        editor: vscode.TextEditor,
-        activePosition: vscode.Position
-    ) {
-        const wordRange =
-            editor.document.getWordRangeAtPosition(activePosition);
+    static fromEditorPosition(editor: vscode.TextEditor, activePosition: vscode.Position) {
+        const wordRange = editor.document.getWordRangeAtPosition(activePosition);
 
-        let selectedWord = undefined;
+        let selectedWord = undefined
 
         if (wordRange !== undefined) {
             // throw new Error('No property found. Please select a property to use this extension.');
             selectedWord = editor.document.getText(wordRange);
-        } else if (selectedWord === undefined || selectedWord[0] !== "$") {
-            const matchWord = editor.document
-                .lineAt(activePosition.line)
-                .text.match(/\$[a-zA-Z_]*/);
+        } else if (selectedWord === undefined || selectedWord[0] !== '$') {
+            const matchWord = editor.document.lineAt(activePosition.line).text.match(/\$[a-zA-Z_]*/);
 
             if (null === matchWord) {
-                throw new Error(
-                    "No property found. Please select a property to use this extension."
-                );
+                throw new Error('No property found. Please select a property to use this extension.');
             }
             selectedWord = matchWord[0];
         } else {
-            throw new Error(
-                "No property found. Please select a property to use this extension."
-            );
+            throw new Error('No property found. Please select a property to use this extension.');
         }
-        let property = new Property(
-            selectedWord.substring(1, selectedWord.length)
-        );
+        let property = new Property(selectedWord.substring(1, selectedWord.length));
 
         const activeLineNumber = activePosition.line;
         const activeLine = editor.document.lineAt(activeLineNumber);
-        const activeLineTokens = activeLine.text.slice(0, -1).split(" ");
-        const typehint =
-            activeLineTokens[activeLineTokens.indexOf(selectedWord) - 1];
+        const activeLineTokens = activeLine.text.slice(0, -1).split(' ');
+        const typehint = activeLineTokens[activeLineTokens.indexOf(selectedWord) - 1];
 
-        if (
-            typehint !== "public" &&
-            typehint !== "private" &&
-            typehint !== "protected"
-        ) {
+        if (typehint !== 'public' && typehint !== 'private' && typehint !== 'protected') {
             property.setType(typehint);
         }
 
-        property.indentation = activeLine.text.substring(
-            0,
-            activeLine.firstNonWhitespaceCharacterIndex
-        );
+        property.indentation = activeLine.text.substring(0, activeLine.firstNonWhitespaceCharacterIndex);
 
         const previousLineNumber = activeLineNumber - 1;
 
@@ -122,7 +95,7 @@ export default class Property {
         const previousLine = editor.document.lineAt(previousLineNumber);
 
         // No doc block found
-        if (!previousLine.text.endsWith("*/")) {
+        if (!previousLine.text.endsWith('*/')) {
             return property;
         }
 
@@ -135,16 +108,16 @@ export default class Property {
             const text = editor.document.lineAt(line).text;
 
             // Reached the end of the doc block
-            if (text.includes("/**") || !text.includes("*")) {
+            if (text.includes('/**') || !text.includes('*')) {
                 break;
             }
 
             // Remove spaces & tabs
-            const lineParts = text.split(" ").filter(function (value) {
-                return value !== "" && value !== "\t" && value !== "*";
+            const lineParts = text.split(' ').filter(function(value){
+                return value !== '' && value !== "\t" && value !== "*";
             });
 
-            const varPosition = lineParts.indexOf("@var");
+            const varPosition = lineParts.indexOf('@var');
 
             // Found @var line
             if (-1 !== varPosition) {
@@ -161,7 +134,7 @@ export default class Property {
 
             const posibleDescription = lineParts.join(` `);
 
-            if (posibleDescription[0] !== "@") {
+            if (posibleDescription[0] !== '@') {
                 property.description = posibleDescription;
             }
         }
@@ -173,73 +146,66 @@ export default class Property {
         return Property.fromEditorPosition(editor, editor.selection.active);
     }
 
-    generateMethodDescription(prefix: string): string {
+    generateMethodDescription(prefix : string) : string {
         if (this.description) {
-            return (
-                prefix +
-                this.description.charAt(0).toLowerCase() +
-                this.description.substring(1)
-            );
+            return prefix + this.description.charAt(0).toLowerCase() + this.description.substring(1);
         }
 
         return prefix + `the value of ` + this.name;
     }
 
-    generateMethodName(prefix: string): string {
-        let name = this.name
-            .split("_")
-            .map((str) => str.charAt(0).toLocaleUpperCase() + str.slice(1))
-            .join("");
+    generateMethodName(prefix : string) : string {
+        let name = this.name.split('_')
+            .map(str => str.charAt(0).toLocaleUpperCase() + str.slice(1))
+            .join('');
         return prefix + name;
     }
 
-    getDescription(): string {
+    getDescription() : string {
         return this.description;
     }
 
-    getIndentation(): string {
+    getIndentation() : string {
         return this.indentation;
     }
 
-    getName(): string {
+    getName() : string {
         return this.name;
     }
 
-    getterDescription(): string {
-        return this.generateMethodDescription("Get ");
+    getterDescription() : string {
+        return this.generateMethodDescription('Get ');
     }
 
-    getterName(): string {
-        return this.generateMethodName(this.type === "bool" ? "is" : "get");
+    getterName() : string {
+        return this.generateMethodName(this.type === 'bool' ? 'is' : 'get');
     }
 
-    getType(): string {
+    getType() : string {
         return this.type;
     }
 
-    getTypeHint(): string {
+    getTypeHint() : string {
         return this.typeHint;
     }
 
-    isValidTypeHint(type: string) {
-        return (
-            -1 === type.indexOf("|") && -1 === this.pseudoTypes.indexOf(type)
-        );
+    isValidTypeHint(type : string) {
+        return (-1 === type.indexOf('|') && -1 === this.pseudoTypes.indexOf(type));
     }
 
-    setterDescription(): string {
-        return this.generateMethodDescription("Set ");
+    setterDescription() : string {
+        return this.generateMethodDescription('Set ');
     }
 
-    setterName(): string {
-        return this.generateMethodName("set");
+    setterName() : string {
+        return this.generateMethodName('set');
     }
 
-    setType(type: string) {
+    setType(type : string) {
         this.type = type;
 
-        if (type.indexOf("[]") > 0) {
-            type = "array";
+        if (type.indexOf('[]') > 0) {
+            type = 'array';
         }
 
         if (this.isValidTypeHint(type)) {
